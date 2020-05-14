311106a4b08e2e5464b213e27efae0510a9f46be
BrickMyself <2457212911@qq.com>
2020-05-14 22:45:57 +0800

    提交练习

--- 2020_5_14Test.py
+++ 2020_5_14Test.py
@@ -1,3 +1,6 @@
+#任务一：创建一个文本文件 Py7-1.txt。内容为“秦时明月汉时关，秦时明月汉时关。但使龙
+#城飞将在，不教胡马度阴山。”保存在当前目录中，使用 writelines()方法将第二句
+#“秦时明月汉时关”改为“万里长征人未还”。
 def create_text(filename):
     path = 'C:/file1/'
     file_path = path + filename + '.txt'
@@ -11,7 +14,10 @@ def create_text(filename):
     f.writelines(flist)
 
 
-
+#任务二：将下列数据输入到 ex06.xlsx 文件中。
+#S09，女，89.2，88.4，86，87.9
+#S10，女，90.5，86.3，87，87.9
+#S11，男，88.7，89.4，89，89.0
 #create_text('Py7-1')
 from openpyxl import Workbook
 def WriteData():
